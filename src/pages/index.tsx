88c1306e1291c910f13b8ab20b212581a44296ce
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-11-17 23:21:08 +0100

    hook to toggle state based on key press

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,35 @@ import { Box, CSSReset, Text, ThemeProvider } from '@chakra-ui/core'
 import * as React from 'react'
 import { useEffect, useReducer } from 'react'
 
-type TheState = { toggled: boolean }
-const initialState: TheState = { toggled: false }
+type Modifier = 'Control' | 'Meta' | 'Shift' | 'Alt'
 
-const reducer = (state: TheState) => {
-  return { toggled: !state.toggled }
+type Key = {
+  key: string,
+  modifiers: Modifier[]
 }
+
+const key = (key: string, modifiers: Modifier[] = []) => {
+  return {
+    key, modifiers,
+  }
+}
+
+const modifierPresent = {
+  'Control': (event: KeyboardEvent) => event.ctrlKey,
+  'Meta': (event: KeyboardEvent) => event.metaKey,
+  'Shift': (event: KeyboardEvent) => event.shiftKey,
+  'Alt': (event: KeyboardEvent) => event.altKey,
+}
+
+
+type State = {
+  toggled: boolean
+}
+const initialState: State = {
+  toggled: false,
+}
+
+const reducer = (previousState: State): State => ({ toggled: !previousState.toggled })
 
 /*
  why not use useState() here, because it does not play nice with the useEffect hook
@@ -17,15 +40,25 @@ const reducer = (state: TheState) => {
  In booth cases the logic will be executed multiple times and not only
  on mount/unmount.
  */
-const useKeyToggle = (targetKey: string) => {
+const useKeyToggle = (toggleKey: Key) => {
   const [state, dispatch] = useReducer(reducer, initialState)
-
-  const downHandler = ({ key }: KeyboardEvent) => {
-    if (key === targetKey) {
-      dispatch('not important')
+  const downHandler = (event: KeyboardEvent) => {
+    const { key, altKey, shiftKey, metaKey, ctrlKey } = event
+    console.log([
+      `key     : ${key}`,
+      `alt     : ${altKey}`,
+      `meta    : ${metaKey}`,
+      `ctrlKey : ${ctrlKey}`,
+      `shiftKey: ${shiftKey}`
+    ].join('\n'))
+    if (toggleKey.key !== key) {
+      return
+    }
+    let allRequestedModifiersPresent = toggleKey.modifiers.reduce((acc: boolean, modifier: Modifier) => acc && modifierPresent[modifier](event), true)
+    if (allRequestedModifiersPresent) {
+      dispatch('does not matter')
     }
   }
-
   useEffect(() => {
     window.addEventListener('keydown', downHandler)
     return () => {
@@ -36,8 +69,7 @@ const useKeyToggle = (targetKey: string) => {
 }
 
 const IndexPage = () => {
-  let pressed = useKeyToggle('Escape')
-
+  let pressed = useKeyToggle(key('Escape', ['Alt', 'Control']))
   return (
     <ThemeProvider>
       <CSSReset/>
