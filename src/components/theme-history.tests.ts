fd7d23103b55e6c8eb9dc6ec1196d6162a2c7069
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-09-27 01:24:04 +0200

    better names

--- src/components/theme-history.tests.ts
+++ src/components/theme-history.tests.ts
@@ -4,7 +4,7 @@ const _1st = randomTheme()
 const _2nd = randomTheme()
 const _3rd = randomTheme()
 
-const prov = () => {
+const preRecordedThemes = () => {
   const all = [_1st, _2nd, _3rd]
   let next = 0
 
@@ -16,15 +16,15 @@ const prov = () => {
 }
 
 describe("ThemeHistory", () => {
-  test("after creation there is a current color", () => {
-    const themeHistory = new ThemeHistory(prov());
+  test("after creation there is a current current", () => {
+    const themeHistory = new ThemeHistory(preRecordedThemes());
     const current = themeHistory.current
 
     expect(current).toBe(_1st)
   });
 
-  test("next creates new random colors", () => {
-    const themeHistory = new ThemeHistory(prov());
+  test("next creates new theme", () => {
+    const themeHistory = new ThemeHistory(preRecordedThemes());
     const newTheme = themeHistory.next()
     const currentTheme = themeHistory.current
 
